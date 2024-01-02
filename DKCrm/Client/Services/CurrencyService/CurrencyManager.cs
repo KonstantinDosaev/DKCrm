9eb02da373a1cbb49e3c1d7e0dbb359f1af8688a
Константин Досаев <dosaevk@yandex.ru>
2024-01-03 00:23:16 +0300

    converter currency

--- DKCrm/Client/Services/CurrencyService/CurrencyManager.cs
+++ DKCrm/Client/Services/CurrencyService/CurrencyManager.cs
@@ -12,19 +12,64 @@ namespace DKCrm.Client.Services.CurrencyService
         {
             _httpClient = httpClient;
         }
-        public async Task<List<(string charCode,string name)>> GetCurrencyCharCode()
+
+        public async Task<IEnumerable<XElement>> GetCurrencyXml()
         {
-            var result = new List<(string charCode, string name)>(){("RUB","Российский рубль")};
             var xml = await _httpClient.GetByteArrayAsync("https://www.cbr-xml-daily.ru/daily.xml");
             var utfString = CodePagesEncodingProvider.Instance.GetEncoding(1251)!.GetString(xml);
             var xdoc = XDocument.Parse(utfString);
-            var el = xdoc.Element("ValCurs").Elements("Valute");
-            foreach (var item in el)
+            var el = xdoc.Element("ValCurs")!.Elements("Valute");
+            
+            return el;
+        }
+        public async Task<List<(string charCode,string name)>> GetCurrencyCharCode()
+        {
+            var result = new List<(string charCode, string name)>(){("RUB","Российский рубль")};
+            var elements = await GetCurrencyXml();
+            foreach (var item in elements)
             {
                 result.Add((item.Element("CharCode")!.Value, item.Element("Name")!.Value.ToString(CultureInfo.InstalledUICulture)));
             }
            
             return result.OrderBy(o => o.charCode).ToList();
+        }
+
+        public async Task<List<(string charCod, decimal?)>> CurrencyConverter(string firstCurrency, decimal? firstPrice,
+            double currencyPercent,
+            IEnumerable<string> currencyCharCodesToConvert)
+        {
+            var result = new List<(string charCod, decimal? value)>() { };
+            var elements = await GetCurrencyXml();
+
+            var cbCharValue = elements.Select(s =>new{
+            
+                charCode=s.Element("CharCode")!.Value,
+                value =Convert.ToDecimal(s.Element("Value")!.Value)
+            }).Where(w=>currencyCharCodesToConvert.Contains(w.charCode)||w.charCode==firstCurrency).ToList();
+            foreach (var charCod in currencyCharCodesToConvert)
+            {
+                if (firstCurrency=="RUB")
+                {
+                    var cource = 1/cbCharValue.FirstOrDefault(f=>f.charCode==charCod)!.value;
+                    result.Add((charCod, firstPrice*(cource+(decimal)currencyPercent)));
+                }
+                else
+                {
+                    decimal cource;
+                    if (charCod=="RUB")
+                    {
+                         cource =  cbCharValue.FirstOrDefault(f => f.charCode == firstCurrency)!.value;
+                    }
+                    else
+                    {
+                        cource = cbCharValue.FirstOrDefault(f => f.charCode == firstCurrency)!.value /
+                            cbCharValue.FirstOrDefault(f => f.charCode == charCod)!.value;
+                    }
+                    result.Add((charCod, firstPrice*(cource + (decimal)currencyPercent)));
+                }
+            }
+
+            return result;
         }
     }
 
--- DKCrm/Client/Services/CurrencyService/ICurrencyManager.cs
+++ DKCrm/Client/Services/CurrencyService/ICurrencyManager.cs
@@ -5,5 +5,8 @@ namespace DKCrm.Client.Services.CurrencyService
     public interface ICurrencyManager
     {
         Task<List<(string charCode, string name)>> GetCurrencyCharCode();
+
+        Task<List<(string charCod, decimal?)>> CurrencyConverter(string firstCurrency, decimal? firstPrice,
+            double currencyPercent, IEnumerable<string> currencyCharCodesToConvert);
     }
 }
